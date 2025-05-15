550a3fe9a8bf1c4bf63bc76388fa3fc8d4c16381
TravisLeblanc1 <travis.leblanc3002@gmail.com>
2025-05-15 08:27:06 +0200

    debugging dep and option removed

--- lib/tier.ml
+++ lib/tier.ml
@@ -25,20 +25,17 @@ let print_classes funs (funUFmap:funUFmap) = List.iter (fun f -> print_classes_f
 let add2venv xlist (tlist:ufelem list) (venv:venv) :venv = 
   List.fold_left2 (fun acc x t -> SMap.add x t acc ) venv xlist tlist
 
-let make_id fname xname = Printf.sprintf "%s_%s" fname xname
-let make_id_res fname = Printf.sprintf "%s_res" fname
+let make_tmp_id () = "E"
+let make_id fname xname = Printf.sprintf "%s%s" fname xname
+let make_id_res fname = Printf.sprintf "%sR" fname
 
 let merge_name s1 s2 = s1^"__"^s2
 
-let merge_classes u1opt u2opt = 
-  if Option.is_some u1opt && Option.is_some u2opt then 
-    let u1 = Option.get u1opt in 
-    let u2 = Option.get u2opt in 
-    UF.merge merge_name u1 u2 |> ignore
+let merge_classes u1 u2 = UF.merge merge_name u1 u2 |> ignore
 
 
 let union_list ulist ulist' = 
-  List.iter2 (fun u v -> merge_classes (Some(u)) v) ulist ulist' (*TODO un peu random le Some...*)
+  List.iter2 (fun u v -> merge_classes u v) ulist ulist' (*TODO un peu random le Some...*)
 
 
 
@@ -62,19 +59,23 @@ let tier_prog (verbose:bool) (prog:prog):unit=
     add2venv f.param (List.tl @@ SMap.find f.name funUFmap) SMap.empty 
   in 
 
-  let rec equiv_expr (fname:string) (venv:venv) expr :ufelem option = 
+  let rec equiv_expr (fname:string) (venv:venv) expr :ufelem= 
     match expr with
-    | Var(x) -> SMap.find_opt x venv
-
+    | Var(x) -> (
+      try 
+        SMap.find x venv
+      with 
+        Not_found -> failwith @@"variable "^x^ " does not exists" )
     | Let(x,e1,e2) ->
       let u = equiv_expr fname venv e1 in 
-      let ux = Some(UF.make (make_id fname x)) in 
+      let ux = UF.make (make_id fname x) in 
       merge_classes ux u |> ignore;
-      equiv_expr fname venv e2
+      let newvenv = SMap.add x ux venv in  
+      equiv_expr fname newvenv e2
 
     | Cstr(_,elst) -> 
       if List.is_empty elst then 
-        None
+        UF.make @@ make_tmp_id ()
       else
         let u = equiv_expr fname venv (List.hd elst) in 
         List.iter (fun e -> let u' = equiv_expr fname venv e in ignore @@ merge_classes  u' u) (List.tl elst);
@@ -84,7 +85,7 @@ let tier_prog (verbose:bool) (prog:prog):unit=
       let ulist = SMap.find f funUFmap in 
       let ulist' = equiv_exprlist fname venv elist in 
       union_list (List.tl ulist) ulist';   (*tl ulist contain the params elem of f*)
-      Some(List.hd ulist)                        (*hd ulist contain the return elem of f*)
+      List.hd ulist                        (*hd ulist contain the return elem of f*)
 
     | Match(e,blist) -> 
       let u = equiv_expr fname venv e in
@@ -94,19 +95,16 @@ let tier_prog (verbose:bool) (prog:prog):unit=
         (List.tl blist);
       bu
 
-  and equiv_exprlist (fname:string) (venv:venv) (elist:expr list) = 
-    List.fold_right (fun e acc -> equiv_expr fname venv e :: acc) elist [] 
+  and equiv_exprlist (fname:string) (venv:venv) (elist:expr list) :ufelem list = 
+    List.map (fun e -> equiv_expr fname venv e) elist
 
   and equiv_branch (fname:string) (venv:venv) u (b:type_branch)= 
     let Branch((_,xlist),e) = b in  (*branch of the form _(xlist) -> e*)
     (* create a new element for each x + merge it with u + and add it to venv*)
     let majvenv (venv:venv) x = 
-      let u' = Some(UF.make (make_id fname x)) in 
+      let u' = UF.make (make_id fname x) in 
       merge_classes  u' u ;
-      if Option.is_some u' then 
-        SMap.add x (Option.get u') venv 
-      else
-        venv
+      SMap.add x u' venv 
     in
     let newvenv = List.fold_left majvenv venv xlist in 
     equiv_expr fname newvenv e
@@ -118,7 +116,7 @@ let tier_prog (verbose:bool) (prog:prog):unit=
     let venv = create_venv f in 
     let u = equiv_expr f.name venv f.body in 
     let ures = List.hd @@ SMap.find f.name funUFmap in 
-    merge_classes u (Some(ures));
+    merge_classes u ures;
     print_string @@"--------- " ^ f.name ^ " ------\n";
     print_classes prog.fundefs funUFmap;
   in 
